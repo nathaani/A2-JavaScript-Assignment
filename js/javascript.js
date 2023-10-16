8c9a075192f3d7744deffdb74837a751e4e0e922
nathaani <144153847+nathaani@users.noreply.github.com>
2023-10-16 13:53:56 -0400

    Reading assistance & Keyboard shortcut
    - Add an HTML element that allows a use to see a high-contrast version of the site. This element should act as a "toggle" button. This would entail using high-contrast colours on the site instead of the ones we already used in the CSS assignment. Consider background colours and font colors.
    - Add more padding around elements and increase the line height and letter spacing.
    
    - Add a keyboard shortcut, "Ctrl + A", that would toggle Reading Assistance on and off.

---
+++ js/javascript.js
@@ -0,0 +1,90 @@
+//COOKIES//////////////////////////////////////////////////////////////////////
+let footer = document.querySelector(".footer");
+let cookies = document.querySelector(".cookies");
+let cookiesText = document.querySelector(".cookiesText");
+
+let click = false;
+
+cookies.innerHTML = '<a href="#" class="accept-cookies">Accept Cookies</a>';
+cookiesText.innerHTML = 'This website uses cookies to improve your experiece, by clicking "accept cookies" you consent to our use of cookies.'
+
+cookies.addEventListener("click", whenClicked);
+
+function whenClicked(event) {
+    if (click) {
+        cookies.innerHTML = '<a href="#" class="accept-cookies">Accept Cookies</a>';
+        cookiesText.innerHTML = 'This website uses cookies to improve your experiece, by clicking "accept cookies" you consent to our use of cookies.'
+
+        click = false;
+    } else {
+        cookies.innerHTML = '<a href="#" class="accept-cookies">Revoke Cookies</a>';
+        cookiesText.innerHTML = 'You have accepted the usage of cookies to improve your experiece on this website, would you like to revoke?'
+
+        click = true;
+    }
+}
+//HIGH CONTRAST////////////////////////////////////////////////////////////////////////
+
+let hcButton = false;
+
+let button = document.querySelector(".button");
+
+let hc = document.querySelector(".highContrast");
+
+let links = document.querySelectorAll("a");
+
+button.addEventListener("click", buttonClicked);
+function buttonClicked(event) {
+
+    if (hcButton) {
+        hc.style.backgroundColor = "white";
+        hc.style.color = "black";
+        hc.style.lineHeight = "1.2";
+        hc.style.margin = "0.1";
+        hc.style.padding = "0";
+        
+
+        //loop through each link 
+        links.forEach(link => {
+            link.style.color = "blue";
+        });
+
+        hcButton = false;
+    } else {
+        hc.style.backgroundColor = "black";
+        hc.style.color = "white";
+        hc.style.lineHeight = "2";
+        hc.style.margin = "10px";
+        hc.style.padding = "10px";
+
+        links.forEach(link => {
+            link.style.color = "white";
+        });
+
+        hcButton = true;
+
+    }
+}
+
+document.addEventListener("keydown", function (event) {
+    if (event.ctrlKey && event.key === "a") {
+        if (hcButton) {
+            hc.style.backgroundColor = "white";
+            hc.style.color = "black";
+            hc.style.lineHeight = "1.2";
+            hc.style.margin = "0";
+            hc.style.padding = "0";
+
+            hcButton = false;
+        } else {
+            hc.style.backgroundColor = "black";
+            hc.style.color = "white";
+            hc.style.lineHeight = "2";
+            hc.style.margin = "10px";
+            hc.style.padding = "10px";
+
+            hcButton = true;
+
+        }
+    }
+});
---
+++ js/javascriptTable.js
@@ -0,0 +1,30 @@
+//TABLE VISITORS///////////////////////////////////////////////////////////////
+let monthlyVisitors = {
+    "months": ["April", "May", "June", "July", "August", "September"],
+    "visitors": ["5k", "3k", "500", "2k", "1k", "1k"]
+
+}
+let section = document.querySelector(".section");
+
+let visitorsTable = document.createElement("table");
+visitorsTable.style.border = "1px solid black";
+
+section.appendChild(visitorsTable);
+
+for (let i = 0; i < monthlyVisitors.months.length; i++) {
+    let tableRow = document.createElement("tr");
+    tableRow.style.border = "1px solid black";
+
+    let tableHeader = document.createElement("th");
+    tableHeader.style.border = "1px solid black";
+
+    let tableData = document.createElement("td");
+    tableData.style.border = "1px solid black";
+
+    tableHeader.textContent = monthlyVisitors.months[i];
+    tableData.textContent = monthlyVisitors.visitors[i];
+
+    tableRow.appendChild(tableHeader);
+    tableRow.appendChild(tableData);
+    visitorsTable.appendChild(tableRow);
+}
